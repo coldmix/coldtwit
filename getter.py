3f0dcd97b3bdf651bd2f48f644c637026078808e
Shashank Rao <rshashank192@gmail.com>
2013-07-27 12:54:26 +0530

    Setup API V1.1 with config

--- getter.py
+++ getter.py
@@ -1,9 +1,28 @@
-import json
-import urllib2
+#coldTwit
+import twitter
 
-url= "http://search.twitter.com/search.json?q=narendra+modi"
-data = urllib2.urlopen(url)
-jdata = json.load(data)
+#parsing configuration details from config.dat
+def parseConfigs():	
+	reader = open('config.dat')
+	myarray = []
+	for i in range(0,4):
+		ele = reader.readline()
+		ele = ele[ele.index("'")+1:-2]
+		myarray.append(ele)
+	reader.close()
+	return myarray
 
-#print raw data 
-print jdata
+#connect function which calls config and also returns the api
+def connect():
+	parray = parseConfigs()
+	consumer_key=parray[0]
+	consumer_secret=parray[1]
+	access_token_key=parray[2]
+	access_token_secret=parray[3]
+	api = twitter.Api(consumer_key,consumer_secret,access_token_key,access_token_secret)
+	return api
+
+
+if __name__ == '__main__':
+    api = connect()
+	print api.VerifyCredentials()
